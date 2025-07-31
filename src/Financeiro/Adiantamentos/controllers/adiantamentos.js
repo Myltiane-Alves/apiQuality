9c8e8c4b4e24dc08d40728cf374bbcd93b94f222
Myltiane Alves <myltiane.aux@gmail.com>
2025-07-31 10:28:29 -0300

    refactor: mudando api

--- src/Financeiro/Adiantamentos/controllers/adiantamentos.js
+++ src/Financeiro/Adiantamentos/controllers/adiantamentos.js
@@ -5,23 +5,23 @@ import 'dotenv/config';
 const url = process.env.API_URL;
 
 class AdiantamentosControllers {
-    async getListaAdiantamentoSalarialFinanceiro(req, res) {
-        let { idEmpresa, dataPesquisaInicio, dataPesquisaFim, pageSize, page } = req.query;
-    
-          idEmpresa = idEmpresa ? idEmpresa : '';
-          dataPesquisaInicio = dataPesquisaInicio ? dataPesquisaInicio : '';
-          dataPesquisaFim = dataPesquisaFim ? dataPesquisaFim : '';
-        try {
-          const apiUrl = `${url}/api/financeiro/adiantamento-salarial.xsjs?idEmpresa=${idEmpresa}&dataPesquisaInicio=${dataPesquisaInicio}&dataPesquisaFim=${dataPesquisaFim}`
-          const response = await axios.get(apiUrl);
-          // const response = await getAdiantamentoSalarial(idEmpresa, dataPesquisaInicio, dataPesquisaFim, pageSize, page)
-    
-          return res.json(response.data);
-        } catch (error) {
-          console.error("Unable to connect to the database:", error);
-          throw error;
-        }
+  async getListaAdiantamentoSalarialFinanceiro(req, res) {
+    let { idEmpresa, dataPesquisaInicio, dataPesquisaFim, pageSize, page } = req.query;
+
+    idEmpresa = idEmpresa ? idEmpresa : '';
+    dataPesquisaInicio = dataPesquisaInicio ? dataPesquisaInicio : '';
+    dataPesquisaFim = dataPesquisaFim ? dataPesquisaFim : '';
+    try {
+      const apiUrl = `${url}/api/financeiro/adiantamento-salarial.xsjs?idEmpresa=${idEmpresa}&dataPesquisaInicio=${dataPesquisaInicio}&dataPesquisaFim=${dataPesquisaFim}`
+      const response = await axios.get(apiUrl);
+      // const response = await getAdiantamentoSalarial(idEmpresa, dataPesquisaInicio, dataPesquisaFim, pageSize, page)
+
+      return res.json(response.data);
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+      throw error;
     }
+  }
 
 }
 
