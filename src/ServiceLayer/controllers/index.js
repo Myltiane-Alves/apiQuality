1ac4ae5ff335ded1decf85a7230020c00d5a475f
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-06 16:45:07 -0300

    feat: add ServiceLayerClient introduzindo orientação a objetos com micro-service

--- src/ServiceLayer/controllers/index.js
+++ src/ServiceLayer/controllers/index.js
@@ -1,10 +1,11 @@
 
 import { DepositoService } from "../services/index.js";
 import { ServiceLayerClient } from "../client/index.js";
-
+import axios from "axios";
+import dotenv from "dotenv";
 const sapClient = new ServiceLayerClient(process.env.API_URL);
 const depositoService = new DepositoService(sapClient);
-// let url = `http://164.152.245.77:8000/quality/concentrador_node`;
+let url = `http://164.152.245.77:8000/quality/concentrador_homologacao`;
 
 class ServiceLayerControllers {
 
@@ -18,16 +19,35 @@ class ServiceLayerControllers {
             } else {
                 IDDEPOSITOLOJA = req.body.IDDEPOSITOLOJA;
             }
-            console.log('Corpo da requisição recebido no controller:', req.body);
-            console.log('IDDEPOSITOLOJA recebido no controller:', IDDEPOSITOLOJA);
-            const message = await depositoService.integrarDeposito(IDDEPOSITOLOJA);
 
-            res.status(200).json({ message });
+            const response = await axios.post(`${url}/api/service-layer/deposito/jobs/depositos-integracao.xsjs`, { IDDEPOSITOLOJA })
+
+            res.json(response.data);
         } catch (error) {
             console.error('Erro ao integrar depósito no SAP:', error);
             res.status(500).json({ message: 'Erro ao integrar depósito no SAP.' });
         }
     }
+    // async postDepositoIntegrarNoSAP(req, res) {
+        
+    //     try {
+    //         // Se req.body for um array, pegue o primeiro elemento
+    //         let IDDEPOSITOLOJA;
+    //         if (Array.isArray(req.body) && req.body.length > 0) {
+    //             IDDEPOSITOLOJA = req.body[0].IDDEPOSITOLOJA;
+    //         } else {
+    //             IDDEPOSITOLOJA = req.body.IDDEPOSITOLOJA;
+    //         }
+    //         console.log('Corpo da requisição recebido no controller:', req.body);
+    //         console.log('IDDEPOSITOLOJA recebido no controller:', IDDEPOSITOLOJA);
+    //         const message = await depositoService.integrarDeposito(IDDEPOSITOLOJA);
+
+    //         res.status(200).json({ message });
+    //     } catch (error) {
+    //         console.error('Erro ao integrar depósito no SAP:', error);
+    //         res.status(500).json({ message: 'Erro ao integrar depósito no SAP.' });
+    //     }
+    // }
 }
 
 export default new ServiceLayerControllers();
