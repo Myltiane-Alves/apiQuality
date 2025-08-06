3a984e9edc9fba322656be04c9adb91f045a8d21
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-06 09:38:06 -0300

    feat: add ServiceLayerClient introduzindo orientação a objetos com micro-service

---
+++ src/ServiceLayer/client/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import 'dotenv/config';
+const url = process.env.API_URL;
+
+// let url = `http://164.152.245.77:8000/quality/concentrador_node`;
+
+export class ServiceLayerClient {
+    constructor(baseURL) {
+        this.api = axios.create({
+            baseURL: baseURL || url,
+            timeout: 80000,
+        });
+    }
+
+    async integrarDeposito(IDDEPOSITOLOJA) {
+        return this.api.post(`${url}/api/service-layer/deposito/jobs/depositos-integracao.xsjs`, { IDDEPOSITOLOJA });
+    }
+}
+
+// export default new ServiceLayerClient();
