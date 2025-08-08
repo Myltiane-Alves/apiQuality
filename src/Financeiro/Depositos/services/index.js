6e57a49eedf897c8075a946bffc365e6f57ed842
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-08 14:53:36 -0300

    feat: add DepositoService getListaConciliarBalancoService

--- src/Financeiro/Depositos/services/index.js
+++ src/Financeiro/Depositos/services/index.js
@@ -3,32 +3,34 @@ export class DepositoService {
         this.client = client;
     }
 
-    async getListaConciliarBalancoService () {
-        let {
-            idDeposito, 
-            idConta, 
-            idEmpresa, 
-            dataCompInicio, 
-            dataCompFim, 
-            dataMovInicio, 
-            dataMovFim, 
-            dataPesquisaInicio, 
-            dataPesquisaFim, 
-            pageSize,
-            page
-        } = queryParams;
-        idDeposito = idDeposito ? idDeposito : '';
-        idConta = idConta ? idConta : '';
-        idEmpresa = idEmpresa ? idEmpresa : '';
-        dataCompInicio = dataCompInicio ? dataCompInicio : '';
-        dataCompFim = dataCompFim ? dataCompFim : '';
-        dataMovInicio = dataMovInicio ? dataMovInicio : '';
-        dataMovFim = dataMovFim ? dataMovFim : '';
-        dataPesquisaInicio = dataPesquisaInicio ? dataPesquisaInicio : '';
-        dataPesquisaFim = dataPesquisaFim ? dataPesquisaFim : '';
-        pageSize = pageSize ? pageSize : '';
-        page = page ? page : '';
-    }
+    // async getListaConciliarBalancoService () {
+    //     let {
+    //         idDeposito, 
+    //         idConta, 
+    //         idEmpresa, 
+    //         dataCompInicio, 
+    //         dataCompFim, 
+    //         dataMovInicio, 
+    //         dataMovFim, 
+    //         dataPesquisaInicio, 
+    //         dataPesquisaFim, 
+    //         pageSize,
+    //         page
+    //     } = queryParams;
+    //     idDeposito = idDeposito ? idDeposito : '';
+    //     idConta = idConta ? idConta : '';
+    //     idEmpresa = idEmpresa ? idEmpresa : '';
+    //     dataCompInicio = dataCompInicio ? dataCompInicio : '';
+    //     dataCompFim = dataCompFim ? dataCompFim : '';
+    //     dataMovInicio = dataMovInicio ? dataMovInicio : '';
+    //     dataMovFim = dataMovFim ? dataMovFim : '';
+    //     dataPesquisaInicio = dataPesquisaInicio ? dataPesquisaInicio : '';
+    //     dataPesquisaFim = dataPesquisaFim ? dataPesquisaFim : '';
+    //     pageSize = pageSize ? pageSize : '';
+    //     page = page ? page : '';
+
+    //     const response = 
+    // }
 
     async cancelarDeposito(IDDEPOSITOLOJA) {
         if(!IDDEPOSITOLOJA) {
