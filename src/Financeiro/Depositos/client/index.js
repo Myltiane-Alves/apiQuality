222b54e5af43b2cd6429d65867c5826c7060ebd1
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-08 11:23:33 -0300

    feat: add ServiceLayerControllers

---
+++ src/Financeiro/Depositos/client/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import 'dotenv/config';
+const url = process.env.API_URL;
+
+export class DepositoClient {
+    constructor(baseURL) {
+        this.api = axios.create({
+            baseURL: baseURL || url,
+            timeout: 80000,
+        });
+    }
+    async integrarDeposito(IDDEPOSITOLOJA) {
+        const response = await this.api.post(`${url}/api/service-layer/deposito/jobs/depositos-integracao.xsjs`, IDDEPOSITOLOJA);
+        return response.data;
+    }
+}
