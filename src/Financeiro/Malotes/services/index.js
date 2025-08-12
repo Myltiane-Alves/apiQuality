a2a6135c749163688d7927d42fba991a70a30b31
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-12 13:42:15 -0300

    feat: add MovimentoCaixaService

--- src/Financeiro/Malotes/services/index.js
+++ src/Financeiro/Malotes/services/index.js
@@ -22,4 +22,5 @@ export class MaloteService {
         
         return result;
     }
+    
 }
---
+++ src/Gerencia/MovimentoCaixa/services/index.js
@@ -0,0 +1,5 @@
+export class MovimentoCaixaService {
+    constructor(client) {
+        this.client = client;
+    }
+}
