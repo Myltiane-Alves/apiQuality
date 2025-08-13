c38f902270fd0e3bebe15ab4daa504a970748a18
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-13 09:36:54 -0300

    feat: add Administrativo VendasServices finalizado

--- src/Administrativo/Vendas/services/index.js
+++ src/Administrativo/Vendas/services/index.js
@@ -1,3 +1,21 @@
 export class VendasServices {
-    
+    constructor(client) {
+        this.client = client;
+    }
+
+    async updateVendaPagamento(IDVENDA, STCANCELADO, DTULTIMAALTERACAO, IDFUNCIONARIOCANCELA, TXTMOTIVOCANCELA ) {
+        if(!IDVENDA) {
+            throw new Error('ID da venda é obrigatório.');
+        }
+
+        const result = await this.client.atualizarVendaPagamento(
+            IDVENDA,
+            STCANCELADO, 
+            DTULTIMAALTERACAO, 
+            IDFUNCIONARIOCANCELA, 
+            TXTMOTIVOCANCELA
+        );
+
+        return result;
+    }
 }
