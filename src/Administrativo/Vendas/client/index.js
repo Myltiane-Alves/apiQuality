d217b97da50b8982a1afa1f4f11bbab6346ab0b6
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-13 09:42:02 -0300

    feat: add Administrativo VendasClient

--- src/Administrativo/Vendas/client/index.js
+++ src/Administrativo/Vendas/client/index.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 import 'dotenv/config';
 const url = process.env.API_URL;
 export class VendasClient {
-    constructor(httpClient) {
-        this.httpClient = httpClient;
+    constructor(baseURL) {
+        this.api = axios.create({
+            baseURL: baseURL || url,
+            timeout: 80000
+        });
     }
 
     async atualizarVendaPagamento(IDVENDA, STCANCELADO, DTULTIMAALTERACAO, IDFUNCIONARIOCANCELA, TXTMOTIVOCANCELA) {
         const response = await this.api.put(`${url}/api/administrativo/altera-venda-pagamento.xsjs`, {
+            IDVENDA,
             STCANCELADO,
             DTULTIMAALTERACAO,
             IDFUNCIONARIOCANCELA,
