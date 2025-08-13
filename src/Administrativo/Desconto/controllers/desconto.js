432ffeaf08a5703da6e7eedd9e9f35f85c78b727
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-13 14:19:18 -0300

    feat: add Administrativo alterarVendaPagamentoSchema

--- src/Administrativo/Desconto/controllers/desconto.js
+++ src/Administrativo/Desconto/controllers/desconto.js
@@ -18,12 +18,12 @@ class AdmDescontoControllers {
 
 
         try {
-                                //   /api/administrativo/desconto-motivo-vendas.xsjs?idMarca=1&idEmpresa=&dataInicial=2024-09-19&dataFinal=2024-09-19&dsmotdesc=Convenio&page=1
+
             const apiUrl = `${url}/api/administrativo/desconto-motivo-vendas.xsjs?idMarca=${idGrupo}&idEmpresa=${idEmpresa}&dataInicial=${dataPesquisaInicio}&dataFinal=${dataPesquisaFim}&dsmotdesc=${dsMotivoDesc}`
             const response = await axios.get(apiUrl)
             // const response = await getDescontoMotivoVendas(idEmpresa, idGrupo, dataPesquisaInicio, dataPesquisaFim, dsMotivoDesc, page, pageSize)
 
-            console.log("Response from Desconto Motivo Vendas:", response.data);
+         
             return res.json(response.data);
         } catch (error) {
             console.error("Unable to connect to the database:", error);
