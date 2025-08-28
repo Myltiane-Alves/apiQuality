8f1b2adc58c0303cd631b97a50553354837fbdb8
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-28 09:12:19 -0300

    feat: add administrativo updateVoucherSchema

--- src/Administrativo/Vouchers/schema/useUpdateVoucher.js
+++ src/Administrativo/Vouchers/schema/useUpdateVoucher.js
@@ -6,14 +6,42 @@ const updateVoucherSchema = Joi.object({
     'number.base': 'IDVOUCHER must be a number',
     'any.required': 'IDVOUCHER is a required field'
   }),
-  STATIVO: Joi.string().allow(''),
-  STCANCELADO: Joi.string().allow(''),
-  DSMOTIVOTROCASTATUS: Joi.string().max(255).allow(''),
-  STSTATUS: Joi.string().allow(''),
-  STTIPOTROCA: Joi.string().allow(''),
-  IDFUNCIONARIO: Joi.number().integer().required(),
-  IDEMPRESALOGADA: Joi.number().integer().required(),
-  IDGRUPOEMPRESARIAL: Joi.number().integer().required(),
+  STATIVO: Joi.string().allow('')
+  .messages({
+    'string.base': 'STATIVO must be a string',
+  }),
+  STCANCELADO: Joi.string().allow('')
+  .messages({
+    'string.base': 'STCANCELADO must be a string',
+  }),
+  DSMOTIVOTROCASTATUS: Joi.string().max(255).allow('')
+  .messages({
+    'string.base': 'DSMOTIVOTROCASTATUS must be a string',
+    'string.max': 'DSMOTIVOTROCASTATUS must be at most 255 characters long'
+  }),
+  STSTATUS: Joi.string().allow('')
+  .messages({
+    'string.base': 'STSTATUS must be a string',
+  }),
+  STTIPOTROCA: Joi.string().allow('')
+  .messages({
+    'string.base': 'STTIPOTROCA must be a string',
+  }),
+  IDFUNCIONARIO: Joi.number().integer().required()
+  .messages({
+    'number.base': 'IDFUNCIONARIO must be a number',
+    'any.required': 'IDFUNCIONARIO is a required field'
+  }),
+  IDEMPRESALOGADA: Joi.number().integer().required()
+  .messages({
+    'number.base': 'IDEMPRESALOGADA must be a number',
+    'any.required': 'IDEMPRESALOGADA is a required field'
+  }),
+  IDGRUPOEMPRESARIAL: Joi.number().integer().required()
+  .messages({
+    'number.base': 'IDGRUPOEMPRESARIAL must be a number',
+    'any.required': 'IDGRUPOEMPRESARIAL is a required field'
+  }),
 });
 
 export default updateVoucherSchema;
