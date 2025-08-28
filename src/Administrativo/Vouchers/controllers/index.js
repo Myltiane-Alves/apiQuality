b68365f6abc40932f4560dad5d5999c7044d3973
Myltiane Alves <myltiane.aux@gmail.com>
2025-08-28 09:24:29 -0300

    feat: add administrativo VoucherServices

---
+++ src/Administrativo/Vouchers/controllers/index.js
@@ -0,0 +1,10 @@
+import axios from "axios"; 
+import 'dotenv/config';
+const url = process.env.API_URL;
+
+
+class AdmVouchersControllers {
+    
+}
+
+export default new AdmVouchersControllers();
---
+++ src/Administrativo/Vouchers/routes/routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import AdmVendasControllers from '../controllers/admVendas.js';
+
+
+const admVouchersRoutes = new Router();
+
+// admVendasRoutes.get('/lista-venda-cliente', AdmVendasControllers.getListaVendaCliente);
+
+// admVendasRoutes.post('/alterar-venda-pagamento', AdmVouchersControllers.postAlterarVendasPagamento);
+
+export default admVouchersRoutes;
