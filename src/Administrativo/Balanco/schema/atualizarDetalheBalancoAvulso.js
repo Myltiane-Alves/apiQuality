83843d6101c08c1945f194abc9b44ddf045b3abe
Myltiane Alves <myltiane.aux@gmail.com>
2025-09-02 09:09:55 -0300

    feat: add administrativo updateDetalheBalancoAvulsoSchema

--- src/Administrativo/Balanco/schema/atualizarDetalheBalancoAvulso.js
+++ src/Administrativo/Balanco/schema/atualizarDetalheBalancoAvulso.js
@@ -1,11 +1,32 @@
 import Joi from 'joi';
 
 const updateDetalheBalancoAvulsoSchema = Joi.object({
-     IDEMPRESA: Joi.number().required(),
-     NUMEROCOLETOR: Joi.number().required(),
-     DSCOLETOR: Joi.string().max(255).required(),
-     IDPRODUTO: Joi.number().required(),
+     IDEMPRESA: Joi.number().integer.required()
+     .messages({
+        'number.base': 'O campo IDEMPRESA deve ser um número',
+        'number.integer': 'O campo IDEMPRESA deve ser um número inteiro',
+        'any.required': 'O campo IDEMPRESA é obrigatório'
+     }),
+     NUMEROCOLETOR: Joi.number().required()
+     .messages({
+        'number.base': 'O campo NUMEROCOLETOR deve ser um número',
+        'any.required': 'O campo NUMEROCOLETOR é obrigatório'
+     }),
+     DSCOLETOR: Joi.string().max(255).allow('')
+     .messages({
+        'string.base': 'O campo DSCOLETOR deve ser uma string',
+        'string.max': 'O campo DSCOLETOR deve ter no máximo 255 caracteres'
+     }),
+     IDPRODUTO: Joi.string().required()
+     .messages({
+        'string.base': 'O campo IDPRODUTO deve ser uma string',
+        'any.required': 'O campo IDPRODUTO é obrigatório'
+     }),
      TOTALCONTAGEMGERAL: Joi.number().required()
+     .messages({
+        'number.base': 'O campo TOTALCONTAGEMGERAL deve ser um número',
+        'any.required': 'O campo TOTALCONTAGEMGERAL é obrigatório'
+     })
 })
 
 export default updateDetalheBalancoAvulsoSchema;    
