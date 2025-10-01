12060f8098c591a896e85af35b704499c3c6b15b
Myltiane Alves <myltiane.aux@gmail.com>
2025-10-01 09:43:01 -0300

    feat: menus filhos usuario

--- src/ServiceLayer/controllers/index.js
+++ src/ServiceLayer/controllers/index.js
@@ -3,7 +3,7 @@ import { DepositoService } from "../services/index.js";
 import { ServiceLayerClient } from "../client/index.js";
 const sapClient = new ServiceLayerClient(process.env.API_URL);
 const depositoService = new DepositoService(sapClient);
-
+import axios from "axios";
 class ServiceLayerControllers {
 
     async postDepositoIntegrarNoSAP(req, res) {
@@ -23,6 +23,21 @@ class ServiceLayerControllers {
         } catch (error) {
             console.error('Erro ao integrar depósito no SAP:', error);
             res.status(500).json({ message: 'Erro ao integrar depósito no SAP.' });
+        }
+    }
+
+    async postVoucher(req, res) {
+        try {
+          
+            let { } = req.body;
+   
+
+            const response = await axios.post(`http://164.152.245.77:8000/quality/concentrador/api/service-layer/devolucao/devolucao-produtos-voucher-nova/jobs/gerar-devolucao-rotina-completa.xsjs`)
+
+            return res.status(200).json(response.data);
+        } catch (error) {
+            console.error("Erro no ResumoVoucherControllers.putResumoVoucher:", error);
+            return res.status(400).json({ error: error.message });
         }
     }
 }
