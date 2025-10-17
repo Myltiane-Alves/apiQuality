256232ba174d5223bc0171d4237245030c628ce1
Myltiane Alves <myltiane.aux@gmail.com>
2025-10-17 16:35:15 -0300

    feat:  mudando res.json

--- src/Informatica/ConsultaNFCE/controllers/index.js
+++ src/Informatica/ConsultaNFCE/controllers/index.js
@@ -299,7 +299,8 @@ class ConsultaNfeController {
       // Fazer PUT nas vendas cujo cstat é diferente de '100'
       const putApiUrl = 'http://164.152.245.77:8000/quality/concentrador/api/venda/valida-venda-contingencia.xsjs';
 
-      const response = await axios.get(putApiUrl);
+
+      const response = await axios.put(putApiUrl);
       return res.json(response.data); // Retorna
     } catch (err) {
       return res.status(500).json({ error: err.message });
