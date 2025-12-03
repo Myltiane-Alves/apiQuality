d22b53784ab70dd0bcad15101857be22e42a01c1
Myltiane Alves <myltiane.aux@gmail.com>
2025-12-03 15:26:08 -0300

    refactor:  gerencia

--- src/Gerencia/AlteracaoPreco/controllers/index.js
+++ src/Gerencia/AlteracaoPreco/controllers/index.js
@@ -38,7 +38,7 @@ class GERAlteracaoPrecoControllers {
 
         try {
             
-            const apiUrl = `http://164.152.245.77:8000/quality/concentrador/api/administrativo/alteracao-preco.xsjs?idEmpresa=${idEmpresa}&grupo=${grupo}&subgrupo=${subGrupo}&descProduto=${descProduto}&codBarras=${codBarras}&estoque=${estoque}&dataInicio=${dataPesquisaInicio}&dataFim=${dataPesquisaFim}&page=${page}&pageSize=${pageSize}`;
+            const apiUrl = `${url}/api/administrativo/alteracao-preco.xsjs?idEmpresa=${idEmpresa}&grupo=${grupo}&subgrupo=${subGrupo}&descProduto=${descProduto}&codBarras=${codBarras}&estoque=${estoque}&dataInicio=${dataPesquisaInicio}&dataFim=${dataPesquisaFim}&page=${page}&pageSize=${pageSize}`;
             const response = await axios.get(apiUrl);
             // const response = await getAlteracaoPreco(idMarca, idEmpresa, grupo, subGrupo, descProduto, codBarras, estoque, dataPesquisaInicio, dataPesquisaFim, page, pageSize)
 
