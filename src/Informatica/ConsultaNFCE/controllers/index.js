533d561fcc8cd51427b373ed0d2e754b132a3baf
Myltiane Alves <myltiane.aux@gmail.com>
2025-12-08 12:06:46 -0300

    refactor:  consultaController

--- src/Informatica/ConsultaNFCE/controllers/index.js
+++ src/Informatica/ConsultaNFCE/controllers/index.js
@@ -380,6 +380,16 @@ async validarStatusSefaz(req, res) {
       const xLgr = venda.data[0].NFE_INFNFE_EMIT_ENDEREMIT_XLGR || "Endereco";
       const emit_IE = venda.data[0].NFE_INFNFE_EMIT_IE || "";
       const emit_CRT = venda.data[0].NFE_INFNFE_EMIT_CRT || "1";
+      const cprod = venda.detalhe?.det[0]?.prod?.CPROD || "0001";
+      const cean = venda.detalhe?.det[0]?.prod?.CEAN || "0000000000000";
+      const xprod = venda.detalhe?.det[0]?.prod?.XPROD || "Produto Teste";
+      const ncm = venda.detalhe?.det[0]?.prod?.NCM || "00000000";
+      // const tpCredPresIBSZFM 
+      const CFOP = venda.detalhe?.det[0]?.prod?.CFOP || "5102";
+      const uCom = venda.detalhe?.det[0]?.prod?.UCOM || "UN";
+      const qCom = venda.detalhe?.det[0]?.prod?.QCOM || "1.0000";
+      const vUnCom = venda.detalhe?.det[0]?.prod?.VUNCOM || "0.01";
+      const vProd = venda.detalhe?.det[0]?.prod?.VPROD || "0.01";
       // console.log(venda.data[0]?.venda?.NFE_INFNFE_EMIT_CNPJ, 'venda.produtos')
       // console.log(chave, 'chave')
       const payload = {
@@ -438,9 +448,9 @@ async validarStatusSefaz(req, res) {
           CNPJ: cnpj,
         },
         det: {
-          prod: venda.produtos.map((prod, index) => ({
-            
-          })),
+          prod: {
+            cProd: cprod,
+          }
         },
         icms: {
           orig: "0",
