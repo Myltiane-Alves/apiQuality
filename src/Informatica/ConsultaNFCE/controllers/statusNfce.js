050d3b3992fe386a08eb45f8d289f04742d87d0a
Myltiane Alves <myltiane.aux@gmail.com>
2025-12-29 10:02:00 -0300

    refactor: add ConsultaStatusNfeController

--- src/Informatica/ConsultaNFCE/controllers/statusNfce.js
+++ src/Informatica/ConsultaNFCE/controllers/statusNfce.js
@@ -142,11 +142,8 @@ class ConsultaStatusNfeController {
             {
               mod: "65",
               tpAmb: 2,
-              // UF: 'MT',
               UF: UF,
               versao: "4.00",
-              xmllint: path.resolve("./libs/libxml/bin/xmllint.exe"),
-              openssl: path.resolve("./libs/openssl/bin/openssl.exe"),
             },
             certOptions
           );
@@ -173,131 +170,6 @@ class ConsultaStatusNfeController {
       return res.status(500).json({ error: err.message });
     }
   } 
-
-  async statusSefaz(req, res) {
-    try {
-      let { idVenda } = req.query;
-
-      if (!idVenda) {
-        return res.status(400).json({ error: "idVenda é obrigatório" });
-      }
-
-      const response = await axios.get(`http://164.152.245.77:8000/quality/concentrador_homologacao/api/venda/lista-venda-new-xml.xsjs?id=${idVenda}`);
-      const vendaData = response.data;
-      const configData = response.data.data[0]?.configuracao?.[0]?.config || {};
-      const dsCRT = configData.DSCRT || "";
-      const cscId = configData.IDTOKEN || "1";
-      const csc = configData.TOKENCSC || "";
-      const uf = vendaData.data[0]?.venda.NFE_INFNFE_EMIT_ENDEREMIT_UF;
-      const mod = String(vendaData.data[0]?.venda.NFE_INFNFE_IDE_MOD || "65");
-      const tpAmb = parseInt(vendaData.data[0]?.venda.NFE_INFNFE_IDE_TPAMB || 2);
-
-      const SENHA_CERT = process.env.SENHA || "#senhagto2024#";
-      const certOptions = await getCertOptions(SENHA_CERT, './GTO COMERCIO 2025-2026.pfx');
-      const opensslModulesPath = path.resolve("./libs/openssl/lib/ossl-modules");
-        process.env.OPENSSL_MODULES = opensslModulesPath;
-
-      if (!certOptions) {
-        return res.status(500).json({
-          error: 'Não foi possível carregar o certificado. Verifique as variáveis de ambiente ou o arquivo local.'
-        });
-      }
-      console.log('Consultando status da SEFAZ para UF:', uf, 'Mod:', mod, 'TpAmb:', tpAmb);
-      const tools = new Tools({
-        mod: mod,
-        tpAmb: tpAmb,
-        UF: String(uf),
-        // UF: 'MT',
-        versao: "4.00",
-        CSC: csc,
-        CSCid: cscId,
-        xmllint: path.resolve("./libs/libxml/bin/xmllint.exe"),
-        openssl: path.resolve("./libs/openssl/bin/openssl.exe"),
-      }, certOptions);
-
-      tools.sefazStatus().then(res => {
-        console.log('Status da SEFAZ:', res);
-
-      }).catch(err => {
-        console.error('Erro ao consultar status da SEFAZ:', err.message);
-      })
-
-      return res.json(vendaData);
-    } catch (error) {
-      console.error('Erro ao consultar venda ou gerar XML:', error);
-      return res.status(500).json({ error: 'Erro ao consultar venda ou gerar XML' });
-    }
-  }
-
-  async downloadNFE(req, res) {
-    try {
-      let { idVenda } = req.body;
-
-      if (!idVenda) {
-        return res.status(400).json({ error: "idVenda é obrigatório" });
-      }
-
-      const response = await axios.get(`http://164.152.245.77:8000/quality/concentrador_homologacao/api/venda/lista-venda-new-xml.xsjs?id=${idVenda}`);
-      const vendaData = response.data;
-
-
-      const configData = response.data.data[0]?.configuracao?.[0]?.config || {};
-      const dsCRT = configData.DSCRT || "";
-      const cscId = configData.IDTOKEN || "1";
-      const csc = configData.TOKENCSC || "";
-      const uf = vendaData.data[0]?.venda.NFE_INFNFE_EMIT_ENDEREMIT_UF;
-      const mod = String(vendaData.data[0]?.venda.NFE_INFNFE_IDE_MOD || "55");
-      const tpAmb = parseInt(vendaData.data[0]?.venda.NFE_INFNFE_IDE_TPAMB || "2", 10);
-      const cnpj = vendaData.data[0]?.venda?.NFE_INFNFE_EMIT_CNPJ;
-      const chaveRaw = vendaData.data[0]?.venda.CHAVE || "";
-      const chave = chaveRaw.replace(/^NFe/i, '').replace(/\D/g, '').slice(0, 44);
-
-
-      const SENHA_CERT = process.env.SENHA || "#senhagto2024#";
-      const certOptions = await getCertOptions(SENHA_CERT, './GTO COMERCIO 2025-2026.pfx');
-      const opensslModulesPath = path.resolve("./libs/openssl/lib/ossl-modules");
-        process.env.OPENSSL_MODULES = opensslModulesPath;
-
-      if (!certOptions) {
-        return res.status(500).json({
-          error: 'Não foi possível carregar o certificado. Verifique as variáveis de ambiente ou o arquivo local.'
-        });
-      }
-
-      const tools = new Tools({
-        mod: '55',
-        tpAmb: tpAmb,
-        // UF: 'MT',
-        UF: String(uf),
-        versao: "4.00",
-        CNPJ: cnpj,
-        CSC: csc,
-        CSCid: cscId,
-        xmllint: path.resolve("./libs/libxml/bin/xmllint.exe"),
-        openssl: path.resolve("./libs/openssl/bin/openssl.exe"),
-      }, certOptions);
-  
-      tools.sefazDistDFe({chNFe: chave}).then(res => {
-        console.log('Status da SEFAZ:', res);
-        fs.writeFileSync(`./xml-download/NFe-${chave}.xml.zip`, res);
-        docZip(res)
-          .then(() => {
-            console.log(`Arquivo NFe-${chave}.xml extraído com sucesso!`);
-          })
-          .catch(err => {
-            console.error('Erro ao extrair o arquivo XML:', err.message);
-          });
-      }).catch(err => {
-        console.error('Erro ao consultar status da SEFAZ:', err.message);
-        fs.writeFileSync(`./xml-download/Erro-NFe-${chave}.xml`, JSON.stringify(err, null, 2));
-      })
-
-      return res.json(vendaData);
-    } catch (error) {
-      console.error('Erro ao consultar venda ou gerar XML:', error);
-      return res.status(500).json({ error: 'Erro ao consultar venda ou gerar XML' });
-    }
-  }
 }
 
 export default new ConsultaStatusNfeController();
